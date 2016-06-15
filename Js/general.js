907f0c2fedd623ec00b347d256cf2722f2676008
Nikolay <funeralfear@gmail.com>
2016-06-15 13:12:16 +0300

    Try dashboard with all libraries

---
+++ Js/general.js
@@ -0,0 +1,12 @@
+ jQuery(document).ready(function($) {
+	$("a.mobile").click(function(){
+		$(".sidebar").slideToggle("fast");
+	});
+
+	window.onresize = function(event) {
+		if ($(window).width() > 375) {
+			$(".sidebar").show();
+		}	
+	};
+
+});
